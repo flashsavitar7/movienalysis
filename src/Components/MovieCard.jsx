a42336c3a21d3a25cc9dbc2b27ad6fd171116adb
triumph edet <99418944+flashsavitar7@users.noreply.github.com>
2022-12-13 14:11:32 +0100

    Update MovieCard.jsx

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -5,7 +5,7 @@ function MovieCard({ movie }) {
     <Link to={`/movie/${movie.id}`}>
       <div className='w-[21rem] max-w-[100%] bg-black rounded-xl p-3 text-white m-5 flex flex-col  cursor-pointer text-xl hover:scale-110'>
         <img
-          className='w-full self-center rounded-lg h-[436px]'
+          className='w-full self-center rounded-lg h-[400px]'
           src={"https://image.tmdb.org/t/p/original/" + movie.poster_path}
           alt='poster'
         />
